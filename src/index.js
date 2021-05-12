ae5b81de4dd0d00fc2853cb9dcb3c8d83a1af67e
Oleksandr Simakov <simakov.mailbox@gmail.com>
2021-05-13 00:26:09 +0300

    adds corrections to task2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './styles.css';
 
+
 // задание 1
 const delay = ms => {
     return new Promise((resolve) => {
@@ -8,10 +9,44 @@ const delay = ms => {
         }, ms)
     })
 };
-
 const logger = time => console.log(`Resolved after ${time}ms`);
 
 // Вызовы функции для проверки
 delay(2000).then(logger); // Resolved after 2000ms
 delay(1000).then(logger); // Resolved after 1000ms
 delay(1500).then(logger); // Resolved after 1500ms
+
+
+// задание 2
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+    const updatedUsers = allUsers.map(user =>
+        user.name === userName ? { ...user, active: !user.active } : user,
+    );
+    return new Promise((resolve) => {
+        resolve(updatedUsers)
+    })
+};
+
+const logger2 = updatedUsers => console.table(updatedUsers);
+
+/*
+ * Сейчас работает так
+ */
+// toggleUserState(users, 'Mango', logger2);
+// toggleUserState(users, 'Lux', logger2);
+
+/*
+ * Должно работать так
+ */
+toggleUserState(users, 'Mango').then(logger2);
+toggleUserState(users, 'Lux').then(logger2);
+
+
+// задание 3
