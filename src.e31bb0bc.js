7247caeaa04fe4a8b6c2a66fd66e653a6fe6bbb0
Oleksandr Simakov <simakov.mailbox@gmail.com>
2021-05-13 01:57:11 +0300

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -117,152 +117,105 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
-var bundleURL = null;
+})({"index.js":[function(require,module,exports) {
+// import './styles.css';
+// // задание 1
+// const delay = ms => {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve(ms);
+//         }, ms)
+//     })
+// };
+// const logger = time => console.log(`Resolved after ${time}ms`);
+// // Вызовы функции для проверки
+// delay(2000).then(logger); // Resolved after 2000ms
+// delay(1000).then(logger); // Resolved after 1000ms
+// delay(1500).then(logger); // Resolved after 1500ms
+// // задание 2
+// const users = [
+//   { name: 'Mango', active: true },
+//   { name: 'Poly', active: false },
+//   { name: 'Ajax', active: true },
+//   { name: 'Lux', active: false },
+// ];
+// const toggleUserState = (allUsers, userName) => {
+//     const updatedUsers = allUsers.map(user =>
+//         user.name === userName ? { ...user, active: !user.active } : user,
+//     );
+//     return new Promise((resolve) => {
+//         resolve(updatedUsers)
+//     })
+// };
+// const logger2 = updatedUsers => console.table(updatedUsers);
+// /*
+//  * Сейчас работает так
+//  */
+// // toggleUserState(users, 'Mango', logger2);
+// // toggleUserState(users, 'Lux', logger2);
+// /*
+//  * Должно работать так
+//  */
+// toggleUserState(users, 'Mango').then(logger2);
+// toggleUserState(users, 'Lux').then(logger2);
+// задание 3
+var randomIntegerFromInterval = function randomIntegerFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
-function getBundleURLCached() {
-  if (!bundleURL) {
-    bundleURL = getBundleURL();
-  }
-
-  return bundleURL;
-}
-
-function getBundleURL() {
-  // Attempt to find the URL of the current script and use that as the base URL
-  try {
-    throw new Error();
-  } catch (err) {
-    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
-
-    if (matches) {
-      return getBaseURL(matches[0]);
-    }
-  }
-
-  return '/';
-}
-
-function getBaseURL(url) {
-  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)?\/[^/]+(?:\?.*)?$/, '$1') + '/';
-}
-
-exports.getBundleURL = getBundleURLCached;
-exports.getBaseURL = getBaseURL;
-},{}],"../node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
-var bundle = require('./bundle-url');
-
-function updateLink(link) {
-  var newLink = link.cloneNode();
-
-  newLink.onload = function () {
-    link.remove();
-  };
-
-  newLink.href = link.href.split('?')[0] + '?' + Date.now();
-  link.parentNode.insertBefore(newLink, link.nextSibling);
-}
-
-var cssTimeout = null;
-
-function reloadCSS() {
-  if (cssTimeout) {
-    return;
-  }
-
-  cssTimeout = setTimeout(function () {
-    var links = document.querySelectorAll('link[rel="stylesheet"]');
-
-    for (var i = 0; i < links.length; i++) {
-      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
-        updateLink(links[i]);
-      }
-    }
-
-    cssTimeout = null;
-  }, 50);
-}
-
-module.exports = reloadCSS;
-},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"styles.css":[function(require,module,exports) {
-var reloadCSS = require('_css_loader');
-
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
-"use strict";
-
-require("./styles.css");
-
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) { symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); } keys.push.apply(keys, symbols); } return keys; }
-
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-// задание 1
-var delay = function delay(ms) {
-  return new Promise(function (resolve) {
+var makeTransaction = function makeTransaction(transaction) {
+  return new Promise(function (resolve, reject) {
+    var delay2 = randomIntegerFromInterval(200, 500);
+    console.log(delay2);
     setTimeout(function () {
-      resolve(ms);
-    }, ms);
+      var canProcess = Math.random() > 0.3;
+
+      if (canProcess) {
+        resolve([transaction.id, delay2]);
+      } else {
+        reject(transaction.id);
+      }
+    }, delay2);
   });
 };
 
-var logger = function logger(time) {
-  return console.log("Resolved after ".concat(time, "ms"));
-}; // Вызовы функции для проверки
-
-
-delay(2000).then(logger); // Resolved after 2000ms
-
-delay(1000).then(logger); // Resolved after 1000ms
-
-delay(1500).then(logger); // Resolved after 1500ms
-// задание 2
-
-var users = [{
-  name: 'Mango',
-  active: true
-}, {
-  name: 'Poly',
-  active: false
-}, {
-  name: 'Ajax',
-  active: true
-}, {
-  name: 'Lux',
-  active: false
-}];
-
-var toggleUserState = function toggleUserState(allUsers, userName) {
-  var updatedUsers = allUsers.map(function (user) {
-    return user.name === userName ? _objectSpread(_objectSpread({}, user), {}, {
-      active: !user.active
-    }) : user;
-  });
-  return new Promise(function (resolve) {
-    resolve(updatedUsers);
-  });
+var logSuccess = function logSuccess(id) {
+  console.log("Transaction ".concat(id[0], " processed in ").concat(id[1], "ms"));
 };
 
-var logger2 = function logger2(updatedUsers) {
-  return console.table(updatedUsers);
+var logError = function logError(id) {
+  console.warn("Error processing transaction ".concat(id, ". Please try again later."));
 };
 /*
- * Сейчас работает так
+ * Работает так
  */
-// toggleUserState(users, 'Mango', logger2);
-// toggleUserState(users, 'Lux', logger2);
+// makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
+// makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
+// makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
+// makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
 
 /*
  * Должно работать так
  */
 
 
-toggleUserState(users, 'Mango').then(logger2);
-toggleUserState(users, 'Lux').then(logger2); // задание 3
-},{"./styles.css":"styles.css"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+makeTransaction({
+  id: 70,
+  amount: 150
+}).then(logSuccess).catch(logError);
+makeTransaction({
+  id: 71,
+  amount: 230
+}).then(logSuccess).catch(logError);
+makeTransaction({
+  id: 72,
+  amount: 75
+}).then(logSuccess).catch(logError);
+makeTransaction({
+  id: 73,
+  amount: 100
+}).then(logSuccess).catch(logError);
+},{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -290,7 +243,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58261" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61158" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
